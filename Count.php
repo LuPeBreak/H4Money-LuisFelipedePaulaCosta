713cd52dc96dabd811cab1c0e567692fd7878467
LuPe_BReak <lfbmrj15@gmail.com>
2019-02-09 12:00:29 -0200

    Atualizaçao de href da navbar

--- Count.php
+++ Count.php
@@ -60,7 +60,7 @@
 
 <body>
   <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">H4Money</a>
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/">H4Money</a>
   </nav>
 
   <div class="container-fluid">
